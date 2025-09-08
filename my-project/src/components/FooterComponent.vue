536d344b04848425ba949c1a553488553b5a9890
jong234 <oip9663@naver.com>
2025-09-08 21:48:44 +0900

    footer 만드는 중에 자꾸 땅속으로 들어가는 오류 발생

--- my-project/src/components/FooterComponent.vue
+++ my-project/src/components/FooterComponent.vue
@@ -1,13 +1,35 @@
-<script setup>
+<script >
 
 </script>
 
 <template>
-  <div>
-
+  <div id='wrapper'>
+    <div>
+      content
+    </div>
   </div>
+  <footer>
+  </footer>
 </template>
 
-<style scoped>
-
+<style >
+#wrapper{
+  height: auto;
+  min-height: 100%;
+  padding-bottom: 422px;
+}
+  footer {
+    position: relative;
+    width: 1440px;
+    display: flex;
+    background: black;
+  }
+  .background {
+    position: fixed;
+    display: flex;
+    min-width: 1440px;
+    height: 422px;
+    padding: 32px 104px 64px 104px;
+    background: white;
+  }
 </style>
