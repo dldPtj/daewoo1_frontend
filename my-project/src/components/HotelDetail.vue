0a84105fb18f3737465e022a27eaa43c46f4cb58
dldPtj <eyeseo723@naver.com>
2025-09-18 18:22:15 +0900

    feat: HotelDetail 개요(overview), 잔여객실 완성

--- my-project/src/components/HotelDetail.vue
+++ my-project/src/components/HotelDetail.vue
@@ -48,7 +48,7 @@ export default {
           </div>
           <!--호텔 평점 개수-->
           <div class="hoteldetail-review-count">
-            <span id="review-count">271</span>reviews
+            <span id="review-count">371</span>reviews
           </div>
         </div>
 
@@ -114,42 +114,163 @@ export default {
       <div class="hotel-overview-text">
         반다르 힐리르 지역에 위치한 대형 호텔로 쇼핑몰과 관광지가 가까워 접근성이 뛰어납니다. 실외 수영장, 스파, 피트니스 센터, 그리고 가족 단위 여행객을 위한 키즈 풀까지 갖추고 있어 편리하고 실속 있는 여행을 원하는 분들에게 적합합니다. 넓은 객실과 모던한 시설은 물론, 비즈니스 센터와 회의실도 완비되어 있어 단체 관광이나 기업 연수에도 어울립니다.
       </div>
-      <div class="hotelrating-avg">
+      <div class="hotel-overview-boxes">
         <div class="hotelrating-avg-now">
-
+          <div class="rating">
+            <h1>4.2</h1>
+          </div>
+          <!--호텔 만족도-->
+          <div class="hoteldetail-overview-satisfaction">
+            <h2 id="satisfaction">Very Good</h2>
+          </div>
+          <!--호텔 리뷰 개수-->
+          <div class="hoteldetail-review-count">
+            <span id="review-count">371</span>reviews
+          </div>
         </div>
-        <div class="hoteloverview-1">
 
+        <div class="hotelfeatures">
+          <div class="stars-img">
+            <img src="@/assets/stars.png">
+          </div>
+          <div class="hotel-feature">
+            <span id="feature-1">Near park</span>
+          </div>
         </div>
-        <div class="hoteloverview-2">
-
+        <div class="hotelfeatures">
+          <div class="stars-img">
+            <img src="@/assets/stars.png">
+          </div>
+          <div class="hotel-feature">
+            <span id="feature-2">Near nightlife</span>
+          </div>
         </div>
-        <div class="hoteloverview-3">
-
+        <div class="hotelfeatures">
+          <div class="stars-img">
+            <img src="@/assets/stars.png">
+          </div>
+          <div class="hotel-feature">
+            <span id="feature-3">Near theater</span>
+          </div>
         </div>
-        <div class="hoteloverview-4">
-
+        <div class="hotelfeatures">
+          <div class="stars-img">
+            <img src="@/assets/stars.png">
+          </div>
+          <div class="hotel-feature">
+            <span id="feature-4">Clean Hotel</span>
+          </div>
         </div>
       </div>
     </div>
     <!--호텔 잔여 객실-->
     <div class="hotel-leftrooms">
-      <div>
+      <div class="hotel-leftrooms-title">
         <h3>잔여 객실</h3>
       </div>
-      <div class="leftroom-1">
+      <!--잔여 객실 리스트-->
 
+      <!--첫번째 객실-->
+      <div class="leftroom">
+        <!--객실 리스트 왼쪽 편(객실이미지, 객실타입 및 침대개수)-->
+        <div class="leftroom-sec">
+          <!--객실 이미지-->
+          <div class="leftroom-img">
+            <img src="@/assets/leftroom-1.png">
+          </div>
+          <!--객실 타입-->
+          <div class="leftroom-type">
+            <span id="roomtype">Superior room</span> - <span id="room-view"></span> - <span id="bed-counts">1 double bed or 2 twin beds</span>
+          </div>
+        </div>
+
+        <!--객실 리스트 오른쪽 편(객실가격, 객실예약버튼)-->
+        <div class="leftroom-sec">
+          <div class="room-price">
+            ₩<span id="room-price">240,000</span><small>/night</small>
+          </div>
+          <div class="room-book">
+            <button id="room-book-btn">Book now</button>
+          </div>
+        </div>
       </div>
-      <div class="leftroom-2">
 
+      <!--두번째 객실-->
+      <div class="leftroom">
+        <!--객실 리스트 왼쪽 편(객실이미지, 객실타입 및 침대개수)-->
+        <div class="leftroom-sec">
+          <!--객실 이미지-->
+          <div class="leftroom-img">
+            <img src="@/assets/leftroom-2.png">
+          </div>
+          <!--객실 타입-->
+          <div class="leftroom-type">
+            <span id="roomtype">Superior room</span> - <span id="room-view">City view</span> - <span id="bed-counts">1 double bed or 2 twin beds</span>
+          </div>
+        </div>
+
+        <!--객실 리스트 오른쪽 편(객실가격, 객실예약버튼)-->
+        <div class="leftroom-sec">
+          <div class="room-price">
+            ₩<span id="room-price">240,000</span><small>/night</small>
+          </div>
+          <div class="room-book">
+            <button id="room-book-btn">Book now</button>
+          </div>
+        </div>
       </div>
-      <div class="leftroom-3">
 
+      <!--세번째 객실-->
+      <div class="leftroom">
+        <!--객실 리스트 왼쪽 편(객실이미지, 객실타입 및 침대개수)-->
+        <div class="leftroom-sec">
+          <!--객실 이미지-->
+          <div class="leftroom-img">
+            <img src="@/assets/leftroom-3.png">
+          </div>
+          <!--객실 타입-->
+          <div class="leftroom-type">
+            <span id="roomtype">Superior room</span> - <span id="room-view">City view</span> - <span id="bed-counts">1 double bed or 2 twin beds</span>
+          </div>
+        </div>
+
+        <!--객실 리스트 오른쪽 편(객실가격, 객실예약버튼)-->
+        <div class="leftroom-sec">
+          <div class="room-price">
+            ₩<span id="room-price">240,000</span><small>/night</small>
+          </div>
+          <div class="room-book">
+            <button id="room-book-btn">Book now</button>
+          </div>
+        </div>
       </div>
-      <div class="leftroom-4">
 
+      <!--네번째 객실-->
+      <div class="leftroom">
+        <!--객실 리스트 왼쪽 편(객실이미지, 객실타입 및 침대개수)-->
+        <div class="leftroom-sec">
+          <!--객실 이미지-->
+          <div class="leftroom-img">
+            <img src="@/assets/leftroom-4.png">
+          </div>
+          <!--객실 타입-->
+          <div class="leftroom-type">
+            <span id="roomtype">Superior room</span> - <span id="room-view">City view</span> - <span id="bed-counts">1 double bed or 2 twin beds</span>
+          </div>
+        </div>
+
+        <!--객실 리스트 오른쪽 편(객실가격, 객실예약버튼)-->
+        <div class="leftroom-sec">
+          <div class="room-price">
+            ₩<span id="room-price">240,000</span><small>/night</small>
+          </div>
+          <div class="room-book">
+            <button id="room-book-btn">Book now</button>
+          </div>
+        </div>
       </div>
     </div>
+
     <!--호텔 위치 지도-->
     <div class="hotel-map">
       <div class="hotel-map-top">
@@ -213,11 +334,79 @@ export default {
 </template>
 
 <style>
-.hotel-overview {
+#room-book-btn {
+  border: #8ae6b2 solid 1px;
+  border-radius: 5px;
+  background-color: #8ae6b2;
+  width: 150px;
+  height: 50px;
+}
+.room-book {
+  margin: 0 0 0 50px;
+}
+.room-price {
+  font-size: 30px;
+  margin: auto;
+}
+.leftroom-type {
+  margin: auto 15px;
+}
+.leftroom {
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  margin: 10px auto;
+  padding: 10px;
+  border-bottom: #D9D9D9 solid 1px;
+}
+.leftroom-sec {
+  display: flex;
+}
+.hotel-leftrooms-title {
+  margin: 30px 0 20px 10px;
+  text-align: left;
+  width: 1250px;
+}
+.hotel-leftrooms {
+  margin: auto;
+  max-width: 1250px;
   flex-direction: column;
+}
+.hotelfeatures {
+  border: #8ae6b2 solid 1px;
+  border-radius: 10px;
+  margin: 0 0 0 20px;
+  padding: 15px;
+  width: 150px;
+}
+.hotel-feature {
+  margin: 55px 0 0 0;
+  text-align: left;
+}
+.hoteldetail-overview-satisfaction {
+  margin-top: 30px;
+}
+.hotel-overview-boxes {
+  display: flex;
+  text-align: left;
+  margin: 20px auto;
+  padding-bottom: 50px;
+  border-bottom: #D9D9D9 solid 1px;
+}
+.hotelrating-avg-now {
+  border: #8ae6b2 solid 1px;
+  border-radius: 10px;
+  background-color: #8ae6b2;
+  padding: 15px;
+}
+.hotel-overview-text {
+  text-align: left;
+  margin: 20px auto;
+}
+.hotel-overview-title {
+  margin: 50px 0 10px 0;
+  text-align: left;
+}
+.hotel-overview {
   margin: auto;
   max-width: 1240px;
 }
