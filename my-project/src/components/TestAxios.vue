e8de4b231ef6c53a0277e000348b5f3b33e5ba35
jong234 <oip9663@naver.com>
2025-09-19 21:04:11 +0900

    db에서 데이터 빼오기 성공

--- my-project/src/components/TestAxios.vue
+++ my-project/src/components/TestAxios.vue
@@ -1,32 +1,34 @@
-  <script >
-  import api from "./api/api.js"
-  export default {
-
-    name : 'TestAxois',
-
-    data(){
-      return {
-        list: [],
-        resultData: null,
-      }
-    },
-    async mounted(){
-      const testResult = await api.get("/api/travel-packages/main");
-      this.resultData = testResult;
-      this.list = this.resultData.data.value;
-    }
-  }
-  </script>
-
-  <template>
-    <div>
-      <div v-for="item in list" :key="item">
-        <span>{{ item.id }}</span>
-        <span>{{ item.name }}</span>
-      </div>
+<template>
+  <div>
+    <div v-for="item in list" :key="item">
+      <span>{{ item.id }}</span>
+      <span>{{ item.title }}</span>
     </div>
-  </template>
+  </div>
+</template>
 
-  <style >
+<script>
+// import axios from 'axios';
+import { aTeamApi } from './util/axios';
 
-  </style>
+export default {
+  name: 'TestAxios',
+
+  data() {
+    return {
+      list: [],
+      resultData: null,
+    }
+  },
+
+  async mounted() {
+    const result = await aTeamApi.get('/api/travel-packages/popular');
+    this.resultData = result;
+    this.list = this.resultData.data.data; //data의 첫번째는 서버 전체의 응답을 의미하고 두번째 data는 그 안의 data키를 빼온거임.
+    console.log(this.list);
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- my-project/src/components/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-const api = axios.create({
-    baseURL: "http://localhost:8888",  // 백엔드 REST API prefix
-    timeout: 5000
-})
-
-export default api
