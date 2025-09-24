b37de2cfaf3dacf9c1cd2b7788c9b5aab1421c03
dldPtj <eyeseo723@naver.com>
2025-09-24 17:56:32 +0900

    Update ReservationPage.vue

--- my-project/src/page/ReservationPage.vue
+++ my-project/src/page/ReservationPage.vue
@@ -24,15 +24,65 @@ export default {
   </div>
 
   <div id="ReservationMain">
-    <h1>예약 내역</h1>
+    <div id="ReservationSpace-btw">
+      <h1>예약 내역</h1>
+      <div>
+        <select id="ReservationDropDownStyle">
+          <option value="Upcoming"><b>Upcoming</b></option>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+      </div>
+    </div>
     <div id="ReservationSub">
-      <h3>객실</h3>
+      <img src="../assets/ion_bed.png" id ="bedImage" > <h3>객실</h3>
+    </div>
+    <div class= "ReservationList" >
+      <div class="ReservationImage">
+        <img src="" alt="호텔 이미지가 나옵니다.">
+      </div>
+      <div class="ReservationCheckIn">
+
+      </div>
+
+      <div class ="ReservationCheckOut">
+
+      </div>
     </div>
   </div>
+
+
   <FooterComponent/>
 </template>
 
 <style>
+.ReservationImage{
+  width: 80px;
+  height: 80px;
+  margin: auto 0 auto 24px;
+}
+.ReservationList{
+  margin-top: 16px;
+  width: 1232px;
+  height: 144px;
+  border: #FF8682 solid 1px;
+}
+#ReservationDropDownStyle{
+  margin-top: 20px;
+  display: flex;
+  position: relative;
+  border: white solid 1px;
+  font-size: 15px;
+  font-weight: bold;
+  height: 20px;
+  width: 100px;
+}
+
+#ReservationSpace-btw{
+  display: flex;
+  justify-content: space-between;
+}
 #ReservationMain{
   text-align: left;
   width: 1232px;
@@ -46,5 +96,13 @@ export default {
   width: 1232px;
   height: 56px;
   border: #FF8682 solid 1px;
+}
+#ReservationSub h3 {
+  margin: auto 0;
+}
+#bedImage{
+  margin: auto 0 auto 25px;
+  width: 21px;
+  height: 16px;
 }
 </style>
